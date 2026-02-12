1ccd3fe23f1016624109c0c3690469e933349939
Gil <pratpladevallgil@gmail.com>
2026-02-12 20:38:45 +0100

    fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   // Required for GitHub Pages: base must be /repo-name/ (set via VITE_BASE_PATH in workflow)
-  base: process.env.VITE_BASE_PATH || "/",
+  //base: process.env.VITE_BASE_PATH || "/",
+  base: '/reserve-ai/',
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
